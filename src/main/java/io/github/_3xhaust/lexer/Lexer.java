0cc9545de3f3791c5e8c3835408a8bc177a6b12e
3x-haust <lyusungyun@icloud.com>
2024-07-24 11:10:51 +0900

    feat: if else 토큰화

--- src/main/java/io/github/_3xhaust/lexer/Lexer.java
+++ src/main/java/io/github/_3xhaust/lexer/Lexer.java
@@ -9,6 +9,8 @@ public class Lexer {
     private final String input;
     private final List<Token> tokens = new ArrayList<>();
     private int position = 0;
+    private int line = 1;
+    private int column = 1;
 
     public Lexer(String input) {
         this.input = input;
@@ -19,144 +21,213 @@ public class Lexer {
             char current = input.charAt(position);
 
             if (Character.isDigit(current)) {
-                StringBuilder numberLiteral = new StringBuilder();
-
-                while (position < input.length() && Character.isDigit(input.charAt(position))) {
-                    numberLiteral.append(input.charAt(position));
-                    position++;
-                }
-
-                // 새로운 Token 객체 생성
-                tokens.add(new Token(Token.NUMBER_LITERAL, numberLiteral.toString()));
-            } else if (current == 'p') {
-                if (input.startsWith("print", position)) {
-                    tokens.add(new Token(Token.PRINT));
-                    position += 5;
-                }
+                tokenizeNumber();
             } else if (current == '\"' || current == '\'') {
-                char quote = current;
+                tokenizeStringLiteral(current);
+            } else if (current == 'p') {
+                tokenizePrintKeyword();
+            } else if(current == 'i') {
+                tokenizeIfKeyword();
+            } else if(current == 'e') {
+                tokenizeElseKeyword();
+            } else if (current == '\n' || current == '\r') {
+                line++;
+                column = 1;
                 position++;
-                StringBuilder stringLiteral = new StringBuilder();
-                while (position < input.length() && input.charAt(position) != quote) {
-                    if (input.charAt(position) == '\\') {
-                        position++;
-                        if (position < input.length()) {
-                            char escaped = input.charAt(position);
-                            switch (escaped) {
-                                case '\\':
-                                    stringLiteral.append('\\');
-                                    break;
-                                case 'n':
-                                    stringLiteral.append('\n');
-                                    break;
-                                case 'r':
-                                    stringLiteral.append('\r');
-                                    break;
-                                case 't':
-                                    stringLiteral.append('\t');
-                                    break;
-                                case 'b':
-                                    stringLiteral.append('\b');
-                                    break;
-                                case 'f':
-                                    stringLiteral.append('\f');
-                                    break;
-                                case '\'':
-                                    stringLiteral.append('\'');
-                                    break;
-                                case '\"':
-                                    stringLiteral.append('\"');
-                                    break;
-                                default:
-                                    stringLiteral.append(escaped);
-                            }
-                        }
-                    } else {
-                        stringLiteral.append(input.charAt(position));
-                    }
-                    position++;
-                }
-                position++;
-
-                tokens.add(new Token(Token.STRING_LITERAL, stringLiteral.toString()));
             } else {
-                switch (current) {
-                    case '+':
-                        if (peek() == '+') {
-                            tokens.add(new Token(Token.PLUS_PLUS));
-                            position++;
-                        } else if (peek() == '=') {
-                            tokens.add(new Token(Token.PLUS_EQUAL));
-                            position++;
-                        } else {
-                            tokens.add(new Token(Token.PLUS));
-                        }
-                        break;
-                    case '-':
-                        if (peek() == '-') {
-                            tokens.add(new Token(Token.MINUS_MINUS));
-                            position++;
-                        } else if (peek() == '=') {
-                            tokens.add(new Token(Token.MINUS_EQUAL));
-                            position++;
-                        } else {
-                            tokens.add(new Token(Token.MINUS));
-                        }
-                        break;
-                    case '*':
-                        if (peek() == '=') {
-                            tokens.add(new Token(Token.ASTERISK_EQUAL));
-                            position++;
-                        } else {
-                            tokens.add(new Token(Token.ASTERISK));
-                        }
-                        break;
-                    case '/':
-                        if (peek() == '=') {
-                            tokens.add(new Token(Token.SLASH_EQUAL));
-                            position++;
-                        } else {
-                            tokens.add(new Token(Token.SLASH));
-                        }
-                        break;
-                    case '%':
-                        if (peek() == '=') {
-                            tokens.add(new Token(Token.PERCENT_EQUAL));
-                            position++;
-                        } else {
-                            tokens.add(new Token(Token.PERCENT));
-                        }
-                        break;
-                    case '=':
-                        if (peek() == '=') {
-                            tokens.add(new Token(Token.EQUAL_EQUAL));
-                            position++;
-                        } else {
-                            tokens.add(new Token(Token.EQUAL));
-                        }
-                        break;
-                    case ')':
-                        tokens.add(new Token(Token.RIGHT_PAREN));
-                        break;
-                    case '(':
-                        tokens.add(new Token(Token.LEFT_PAREN));
-                        break;
-                    case '{':
-                        tokens.add(new Token(Token.LEFT_BRACE));
-                        break;
-                    case '}':
-                        tokens.add(new Token(Token.RIGHT_BRACE));
-                        break;
-                }
-                position++;
+                tokenizeSymbol(current);
             }
         }
-        tokens.add(new Token(Token.EOF));
+
+        tokens.add(new Token(Token.EOF, null, line, column));
         return tokens;
     }
 
-    private char peek() {
-        if (position + 1 >= input.length()) return '\0';
-        return input.charAt(position + 1);
+    private void tokenizeNumber() {
+        StringBuilder numberLiteral = new StringBuilder();
+        int startColumn = column;
+        while (position < input.length() && Character.isDigit(input.charAt(position))) {
+            numberLiteral.append(input.charAt(position++));
+            column++;
+        }
+        tokens.add(new Token(Token.NUMBER_LITERAL, numberLiteral.toString(), line, startColumn));
+    }
+
+    private void tokenizePrintKeyword() {
+        if (input.startsWith("print", position)) {
+            tokens.add(new Token(Token.PRINT, null, line, column));
+            position += 5;
+            column += 5;
+        }
+    }
+
+    private void tokenizeIfKeyword() {
+        if (input.startsWith("if", position)) {
+            tokens.add(new Token(Token.IF, null, line, column));
+            position += 2;
+            column += 2;
+        }
+    }
+
+    private void tokenizeElseKeyword() {
+        if (input.startsWith("else", position)) {
+            tokens.add(new Token(Token.ELSE, null, line, column));
+            position += 4;
+            column += 4;
+        }
+    }
+
+    private void tokenizeStringLiteral(char quote) {
+        StringBuilder stringLiteral = new StringBuilder();
+        int startColumn = column;
+        position++; // 시작 따옴표는 skip
+        column++;
+
+        while (position < input.length() && input.charAt(position) != quote) {
+            if (input.charAt(position) == '\\') {
+                position++; // \ escape 문자는 skip
+                column++;
+                if (position < input.length()) {
+                    stringLiteral.append(getEscapedCharacter(input.charAt(position)));
+                    column++;
+                }
+            } else {
+                stringLiteral.append(input.charAt(position));
+                position++;
+                column++;
+            }
+        }
+        position++; // 마지막 따옴표는 skip
+        column++;
+
+        tokens.add(new Token(Token.STRING_LITERAL, stringLiteral.toString(), line, startColumn));
+    }
+
+    private void tokenizeSymbol(char current) {
+        int startColumn = column;
+        switch (current) {
+            case '+':
+                if (peek(1) == '+') {
+                    tokens.add(new Token(Token.PLUS_PLUS, null, line, column));
+                    position++;
+                    column++;
+                } else if (peek(1) == '=') {
+                    tokens.add(new Token(Token.PLUS_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.PLUS, null, line, column));
+                }
+                break;
+            case '-':
+                if (peek(1) == '-') {
+                    tokens.add(new Token(Token.MINUS_MINUS, null, line, column));
+                    position++;
+                    column++;
+                } else if (peek(1) == '=') {
+                    tokens.add(new Token(Token.MINUS_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.MINUS, null, line, column));
+                }
+                break;
+            case '*':
+                if (peek(1) == '=') {
+                    tokens.add(new Token(Token.ASTERISK_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.ASTERISK, null, line, column));
+                }
+                break;
+            case '/':
+                if (peek(1) == '=') {
+                    tokens.add(new Token(Token.SLASH_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.SLASH, null, line, column));
+                }
+                break;
+            case '%':
+                if (peek(1) == '=') {
+                    tokens.add(new Token(Token.PERCENT_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.PERCENT, null, line, column));
+                }
+                break;
+            case '=':
+                if (peek(1) == '=') {
+                    tokens.add(new Token(Token.EQUAL_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.EQUAL, null, line, column));
+                }
+                break;
+            case '!':
+                if (peek(1) == '=') {
+                    tokens.add(new Token(Token.NOT_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.BANG, null, line, column));
+                }
+                break;
+            case '<':
+                if (peek(1) == '=') {
+                    tokens.add(new Token(Token.LESS_THAN_OR_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.LESS_THAN, null, line, column));
+                }
+                break;
+            case '>':
+                if (peek(1) == '=') {
+                    tokens.add(new Token(Token.GREATER_THAN_OR_EQUAL, null, line, column));
+                    position++;
+                    column++;
+                } else {
+                    tokens.add(new Token(Token.GREATER_THAN, null, line, column));
+                }
+                break;
+            case ')':
+                tokens.add(new Token(Token.RIGHT_PAREN, null, line, column));
+                break;
+            case '(':
+                tokens.add(new Token(Token.LEFT_PAREN, null, line, column));
+                break;
+            case '{':
+                tokens.add(new Token(Token.LEFT_BRACE, null, line, column));
+                break;
+            case '}':
+                tokens.add(new Token(Token.RIGHT_BRACE, null, line, column));
+                break;
+        }
+        position++;
+        column++;
+    }
+
+    private char getEscapedCharacter(char escaped) {
+        switch (escaped) {
+            case '\\': return '\\';
+            case 'n': return '\n';
+            case 'r': return '\r';
+            case 't': return '\t';
+            case 'b': return '\b';
+            case 'f': return '\f';
+            case '\'': return '\'';
+            case '\"': return '\"';
+            default: return escaped;
+        }
+    }
+
+    private char peek(int offset) {
+        if (position + offset >= input.length()) return '\0';
+        return input.charAt(position + offset);
     }
 }
